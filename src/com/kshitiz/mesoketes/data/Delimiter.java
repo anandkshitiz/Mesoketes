388b7212e322169a97a1264120fcbfd46b0e8e18
ksanand <ksanand@KSANAND-LAP.oradev.oraclecorp.com>
2017-10-28 01:05:06 +0530

    Added Delimiters.

---
+++ src/com/kshitiz/mesoketes/data/Delimiter.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.kshitiz.mesoketes.data;
+
+/**
+ * @author kshitiz
+ *
+ */
+public class Delimiter {
+	
+	public static final String DAYS_DELIM = "\r\n";
+	public static final String ATTACKS_DELIM = ":";
+	public static final String ATTACK_DELIM = "-";
+	public static final String DAY_DETAIL_DELIM = ";";
+
+}
